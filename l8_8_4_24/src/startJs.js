df7b3d46daee5da8a871d4250ba198d8e50ee201
Micael Gabay <michaelgabay770@gmail.com>
2024-04-08 15:10:25 +0300

    finish lesson 8

---
+++ l8_8_4_24/src/startJs.js
@@ -0,0 +1,61 @@
+// פונקציית הדפסה יכולה לקבל כמה פרמטרים שתרצו
+// console.log("hello world!", "michael");
+
+// פקודת קלט -> מחזירה תמיד סטרינג
+// prompt("enter a number")
+
+// יצירת משתנה
+let v1 = 45;
+// יצירה של קבוע
+const v2=100;
+
+// סוגי ערכים פרימטביים
+
+//? string
+let message="enter a number..."
+// עם גרש אחד זה עדיין נחשב סטרינג
+// let message='enter a number'
+
+//? number
+// אין הבדל בין שלם לעשרוני
+let num=45;
+// let num=-34.2
+
+// ?boolean
+// מכיל נכון או לא נכון
+// let isOk=true;
+let isOk=false;
+
+// ? undefined 
+// משתנה לא מאותחל מחזיק בערך אנדיפיינד
+let v3;
+
+// ?null
+// ערך שמציין ריק בשונה מהערך אנדיפינד שמציין לא מאותחל
+let v4=null;
+
+// ?NaN
+// Not a number
+// נוצר כתוצאה מביצוע פעולה מתמטית על ערך שאינו מתמטי
+let nanV="hey"/3
+
+
+
+// ערכי ה פולס
+
+// false
+// 0
+// NaN
+// ""
+// null
+// undefined
+
+
+
+
+
+
+
+
+
+
