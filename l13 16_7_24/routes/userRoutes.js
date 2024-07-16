5437ca8dd6630c537e25bf2e1688894137d33f98
Micael Gabay <michaelgabay770@gmail.com>
2024-07-16 15:22:45 +0300

    done part c

--- l13 16_7_24/routes/userRoutes.js
+++ l13 16_7_24/routes/userRoutes.js
@@ -1,13 +1,17 @@
 const UserModel = require("../schemas/user");
 const router = require("express").Router()
-
+const bcrypt = require("bcrypt")
 router.post("/register", async (req, res) => {
     try {
+        req.body.password =await bcrypt.hash(req.body.password, 10)
         const newUser = await UserModel.create(req.body)
         res.status(201).json(newUser)
     } catch (error) {
-        if(error.code==11000)
-        res.status(400).send("user already registered")
+        console.log(error)
+        if (error.code == 11000){
+            return res.status(400).send("user already registered")
+        }
+        res.status(500).send("internal error")
     }
 })
 
--- l13 16_7_24/schemas/user.js
+++ l13 16_7_24/schemas/user.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name: String,
     age: Number,
     email: { type: String, unique: true },
-    password: String
+    password: String,
 }, { timestamps: true })
 
 const UserModel = mongoose.model("users", userSchema);
